706df83e1268c632fc320f26eb87effeb16e997e
Anirudh Sriram <hello@techwithanirudh.com>
2025-07-11 05:14:52 +0000

    feat: update package.json to use concurrently for dev scripts

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
 @namespace ${pkg.author.url}
 @author ${pkg.author.name} <${pkg.author.email}> (${pkg.author.url})
 @homepageURL ${pkg.homepage}
-@updateURL ${pkg.repository.url}/releases/latest/download/main.user.css
+@updateURL https://gh-releases.techwithanirudh.com/api/release/techwithanirudh/coolify-tweaks/latest/main.user.css
 @description ${pkg.description}
 @license ${pkg.license}
 ==/UserStyle== */`),
