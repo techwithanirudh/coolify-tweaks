c30adbca9de299052375a8cd05cd622f059c50be
Anirudh Sriram <hello@techwithanirudh.com>
2025-07-24 16:39:14 +0000

    fix: update Dockerfile and devcontainer.json for improved environment setup and package management

---
+++ .devcontainer/scripts/configure-p10k.sh
@@ -0,0 +1,5 @@
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
+echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> "$HOME/.zshrc"
+curl -fsSL https://raw.githubusercontent.com/techwithanirudh/dotfiles/main/zsh/.p10k.zsh -o "$HOME/.p10k.zsh"
