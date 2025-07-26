914220e7531e8aa3285e3fc88acbd29c9908b961
Anirudh Sriram <hello@techwithanirudh.com>
2025-07-26 06:39:12 +0000

    fix: remove duplicate import of postcssPresetEnv in postcss.config.ts

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,6 +1,6 @@
+import postcssPresetEnv from 'postcss-preset-env';
 import autoprefixer from 'autoprefixer';
 import wrapMozDocument from './plugins/postcss/wrap-moz-document';
-import postcssPresetEnv from 'postcss-preset-env';
 
 export default {
   plugins: [
