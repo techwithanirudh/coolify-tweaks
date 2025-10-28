530a9330d24afa74a559288219c14148a1f6a6c7
Anirudh Sriram <hello@techwithanirudh.com>
2025-10-28 04:37:03 +0000

    feat: add Vercel configuration for deployment

--- apps/api/server/utils/css-compiler.ts
+++ apps/api/server/utils/css-compiler.ts
@@ -1,21 +1,21 @@
 import browserslist from "browserslist";
 import {
   browserslistToTargets,
-  transform as lightningTransform,
-} from "lightningcss";
+  transform,
+} from "lightningcss-wasm";
 
 export function transformCss(css: string): string {
   const blQueries =
     browserslist.loadConfig({ path: "apps/style" }) ?? browserslist.defaults;
   const targets = browserslistToTargets(browserslist(blQueries));
 
-  const result = lightningTransform({
+  const { code } = transform({
     filename: "main.user.css",
-    code: Buffer.from(css),
+    code: new TextEncoder().encode(css),
     minify: false,
     sourceMap: false,
     targets,
   });
 
-  return result.code.toString();
+  return new TextDecoder().decode(code);
 }
