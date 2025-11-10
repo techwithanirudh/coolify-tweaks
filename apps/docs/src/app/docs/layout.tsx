12d09322f287cd5eedfeab2c6dde9afc0543923d
Anirudh Sriram <hello@techwithanirudh.com>
2025-11-10 06:16:27 +0000

    refactor: update React and TypeScript dependencies, migrate Next.js config

--- apps/docs/src/app/docs/layout.tsx
+++ apps/docs/src/app/docs/layout.tsx
@@ -29,7 +29,7 @@ export default function Layout({ children }: LayoutProps<'/docs'>) {
               ...option,
               icon: (
                 <div
-                  className="[&_svg]:size-full rounded-lg size-full text-(--tab-color) max-md:bg-(--tab-color)/10 max-md:border max-md:p-1.5"
+                  className='size-full rounded-lg text-(--tab-color) max-md:border max-md:bg-(--tab-color)/10 max-md:p-1.5 [&_svg]:size-full'
                   style={
                     {
                       '--tab-color': color,
