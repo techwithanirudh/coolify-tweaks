d529f12945d5931d59f65f40acb9cd6a032b8eed
Anirudh Sriram <hello@techwithanirudh.com>
2025-11-17 01:38:35 +0000

    feat: add NEXT_RUNTIME environment variable to configuration

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -70,19 +70,10 @@ const NextApp = async () => {
 };
 
 export default withSentryConfig(NextApp, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
   org: "tech-with-anirudh",
-
   project: "coolify-tweaks",
-
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
@@ -90,7 +81,7 @@ export default withSentryConfig(NextApp, {
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  tunnelRoute: "/monitoring",
+  tunnelRoute: true,
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
