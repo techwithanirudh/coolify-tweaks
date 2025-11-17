9f06eb773b704e57a0da06d3a2286bade3aa83ff
Anirudh Sriram <hello@techwithanirudh.com>
2025-11-17 01:50:10 +0000

    feat: add NEXT_RUNTIME environment variable to configuration

--- apps/docs/sentry.edge.config.ts
+++ apps/docs/sentry.edge.config.ts
@@ -6,15 +6,9 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
+  enabled: process.env.NODE_ENV === "production",
   dsn: "https://a453fcb267d9832bc3a9511e93c00aa1@o4506561774551040.ingest.us.sentry.io/4510377696690176",
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
-
-  // Enable logs to be sent to Sentry
   enableLogs: true,
-
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
 });
